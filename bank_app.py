70dd8454b6313f91dfaff8ac2380c7799f9453a7
onnanapiando <onnapiando@gmail.com>
2014-11-26 19:18:18 +0800

    render template index.html

--- bank_app.py
+++ bank_app.py
@@ -1,9 +1,6 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
-
-if __name__ == '__main__':
-    app.run()
+    return render_template ('index.html')
