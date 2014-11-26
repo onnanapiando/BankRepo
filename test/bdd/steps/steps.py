f529b97c0f480f7ab7ab078d66614aa6633a9b11
onnanapiando <onnapiando@gmail.com>
2014-11-26 19:26:17 +0800

    from nose.tools import assert_equal, assert_in

--- test/bdd/steps/steps.py
+++ test/bdd/steps/steps.py
@@ -2,6 +2,7 @@ from lettuce import *
 from nose.tools import assert_equal
 from webtest import TestApp
 from bank_app import app
+from nose.tools import assert_equal, assert_in
 
 @step(u'I visit the homepage')
 def i_visit_the_homepage(step):
