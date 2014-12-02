d8a0de4e2d1ccb07ef4fa38119d344c621140971
onnanapiando <onnapiando@gmail.com>
2014-12-02 13:43:42 +0800

    imported Account from account

--- bank_app.py
+++ bank_app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 from bank.bank import Bank
+from bank.account import Account
 app = Flask(__name__)
 BANK = Bank()
 
@@ -10,4 +11,9 @@ def hello_world():
     return render_template('index.html', balance=balance)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    import cProfile
+
+    account = Account('1111', 50)
+    BANK.add_account(account)
+    cProfile.run('app.run(debug=True)', sort='time')
+
