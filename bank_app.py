edb08ebd29d26e8722f7d3aa8adbc09df0863b62
onnanapiando <onnapiando@gmail.com>
2014-12-07 20:46:17 +0800

    modified bank_app.py file

--- bank_app.py
+++ bank_app.py
@@ -1,11 +1,14 @@
+"""This is a docstring"""
 from flask import Flask, render_template, request
-from bank.bank import Bank
-from bank.account import Account
-app = Flask(__name__)
+from BankRepo.bank.bank import Bank
+from BankRepo.bank.account import Account
+
+APP = Flask(__name__)
 BANK = Bank()
 
-@app.route('/')
+@APP.route('/')
 def hello_world():
+    """Missing function docstring"""
     account_number = request.args.get('account_number')
     balance = BANK.get_account_balance(account_number)
     return render_template('index.html', balance=balance)
@@ -13,7 +16,7 @@ def hello_world():
 if __name__ == '__main__':
     import cProfile
 
-    account = Account('1111', 50)
-    BANK.add_account(account)
-    cProfile.run('app.run(debug=True)', sort='time')
+    #account = Account('1111', 50)
+    BANK.add_account(Account('1111', 50))
+    cProfile.run('APP.run(debug=True)', sort='time')
 
