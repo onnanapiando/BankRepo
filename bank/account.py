014a5265da45df1b5b6c3b58deea73fab32e3afd
onnanapiando <onnapiando@gmail.com>
2014-12-11 20:29:52 +0800

    Trial Debug

--- bank/account.py
+++ bank/account.py
@@ -4,6 +4,7 @@
 class Account(object):
     """ Create Account Class """
     def __init__(self, account_number, balance):
+        """ Initializer """
         self.account_number = account_number
         self.balance = balance
 
