98f4601bede340e53dc522350d1967ec499b2b4b
onnanapiando <onnapiando@gmail.com>
2014-12-11 20:26:07 +0800

    added docstring and whitespaces

--- bank/account.py
+++ bank/account.py
@@ -1,8 +1,13 @@
+""" Account Module """
+
+
 class Account(object):
+    """ Create Account Class """
     def __init__(self, account_number, balance):
         self.account_number = account_number
         self.balance = balance
 
     def existing_account(self, account_number):
-         self.account_number = account_number
-         account_number != 'None'
+        """ Check if account exists """
+        self.account_number = account_number
+        account_number != 'None'
