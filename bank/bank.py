ea0668fa7c3d5e494c9abada96c78a19a6170491
onnanapiando <onnapiando@gmail.com>
2014-12-12 12:25:33 +0800

    added dosctring

--- bank/bank.py
+++ bank/bank.py
@@ -4,13 +4,12 @@
 class Bank(object):
     """ Created Bank Class"""
     def __init__(self):
+        """ Initializer """
         self.accounts = {}
-
 
     def add_account(self, account):
         """ Account Adding Method """
         self.accounts[account.account_number] = account.balance
-
 
     def get_account_balance(self, account_number):
         """ Method that returns account balance """
