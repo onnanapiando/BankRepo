a953d6916c12be92bd0dc7db02862893a490ad5c
onnanapiando <onnapiando@gmail.com>
2014-12-12 09:58:15 +0800

    deleted existing account test

--- test/unit/account_test.py
+++ test/unit/account_test.py
@@ -7,10 +7,6 @@ class TestAccount(unittest.TestCase):
       self.assertEqual(account.account_number, "001")
       self.assertEqual(account.balance, 50)
 
-    def test_account_if_existing(self):
-       account = Account("001", 50)
-       self.assertEqual(account.existing_account(001), None)
-
 if __name__ == '__main__':
     unittest.main()
 
